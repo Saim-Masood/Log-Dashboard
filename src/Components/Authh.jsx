a85d19e77237632fa6c897137082545ea98f38d6
Saim Masood <146344597+Saim-Masood@users.noreply.github.com>
2024-08-04 17:26:05 +0500

    Update Authh.jsx

--- src/Components/Authh.jsx
+++ src/Components/Authh.jsx
@@ -1,33 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Box, Typography, Divider, Grid, Card, CardContent } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { styled } from '@mui/system';
-
-const StyledCard = styled(Card)(({ theme }) => ({
-  maxWidth: 450,
-  margin: 'auto',
-  padding: theme.spacing(2),
-  [theme.breakpoints.up('md')]: {
-    padding: theme.spacing(4),
-  },
-  borderRadius: '10px',
-  boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-}));
 
 const AuthForm = ({ isLogin, onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [fullName, setFullName] = useState(''); // New field
-  const [phoneNumber, setPhoneNumber] = useState(''); // New field
+  const [fullName, setFullName] = useState(''); 
+  const [phoneNumber, setPhoneNumber] = useState(''); 
   const navigate = useNavigate();
-
-  // Clear form fields when switching between login and signup
-  useEffect(() => {
-    setEmail('');
-    setPassword('');
-    setFullName(''); // Reset new field
-    setPhoneNumber(''); // Reset new field
-  }, [isLogin]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -36,16 +16,12 @@ const AuthForm = ({ isLogin, onLogin }) => {
     if (isLogin) {
       const userExists = storedData.find(user => user.email === email && user.password === password);
       if (userExists) {
-        onLogin(); // Set login status
-        navigate('/dashboard'); // Redirect to dashboard on successful login
+        onLogin(); 
+        navigate('/dashboard'); 
       } else {
         alert('Invalid credentials');
       }
     } else {
-      // localStorage.setItem('users', JSON.stringify([...storedData, { email, password, fullName, phoneNumber }]));
-      // navigate('/login'); // Redirect to login page after successful signup
-      const newUser = { email, password, fullName, phoneNumber };
-      const updatedUsers = [...storedData, newUser];
       localStorage.setItem('users', JSON.stringify(updatedUsers));
       navigate('/login');
     }
