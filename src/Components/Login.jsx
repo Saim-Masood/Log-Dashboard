95abb50850e15652c713c4e324a7740892b842d7
Saim Masood <146344597+Saim-Masood@users.noreply.github.com>
2024-08-04 17:21:53 +0500

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,9 +6,6 @@ import { Grid} from '@mui/material';
 const Login = () => {
     return (
       <Grid container component="main" style={{ height: '100vh'  }}>
-        {/* <Grid item xs = {false} md={6}>
-          <ImageBox />
-        </Grid> */}
         <Grid item xs={12}  >
           <AuthForm isLogin={true} />
         </Grid>
