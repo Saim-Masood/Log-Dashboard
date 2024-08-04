062e875c7edfe14fb92a92532004c2cd7a79e322
Saim Masood <146344597+Saim-Masood@users.noreply.github.com>
2024-08-04 17:21:27 +0500

    Update Signupp.jsx

--- src/Components/Signupp.jsx
+++ src/Components/Signupp.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import AuthForm from './Authh';
-// import { styled } from '@mui/system';
-
-// const ImageBox = styled(Box)(({ theme }) => ({
-//   height: '100vh',
-//   backgroundSize: 'auto 20px',
-//   backgroundPosition: 'center',
-//   [theme.breakpoints.down('md')]: {
-//     display: 'none',
-//   },
-// }));
 
 const Signup = () => {
   return (
     <Grid container component="main" style={{ height: '100vh' }}>
-      {/* <Grid item  xs={false} md={6}>
-        <ImageBox />
-      </Grid> */}
       <Grid item xs={12} >
         <AuthForm isLogin={false} />
       </Grid>
