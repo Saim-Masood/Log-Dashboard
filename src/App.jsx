97aae2531f97046794c37c2136635ef53e8a6e7a
Saim Masood <146344597+Saim-Masood@users.noreply.github.com>
2024-08-04 17:28:07 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AuthForm from './Components/Authh';
-import Dashboard from './Components/Dashboard'; // Import Dashboard component
+import Dashboard from './Components/Dashboard';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    // Check login status from localStorage
-    const loggedInStatus = localStorage.getItem('isLoggedIn');
-    setIsLoggedIn(loggedInStatus === 'true');
-  }, []);
 
   const handleLogin = () => {
     localStorage.setItem('isLoggedIn', 'true');
@@ -18,18 +13,11 @@ const App = () => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
   return (
     <Router>
-      <Routes>
-        {/* Redirect to login if not logged in */}
-        <Route
-          path="/"
-          element={isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />}
-        />
         <Route
           path="/login"
           element={isLoggedIn ? <Navigate to="/dashboard" /> : <AuthForm isLogin={true} onLogin={handleLogin} />}
